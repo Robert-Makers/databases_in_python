8b6919cae60f29ece1a1ffc8452eb8c13b917876
Robert Taylor <rjt903@gmail.com>
2023-11-28 17:53:57 +0000

    completed phase 1 part 1 exercise

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest #type: ignore
-from lib.database_connection import DatabaseConnection
+from lib.ml_database_connection import DatabaseConnection
 
 # This is a Pytest fixture.
 # It creates an object that we can use in our tests.
--- tests/test_album.py
+++ tests/test_album.py
@@ -4,8 +4,28 @@ from lib.album import Album
 Album constructs with a title, release year, artist id and id
 '''
 def test_albums_constructs():
-    album = Album(1, 'Artpop', 2013, 'Pop')
+    album = Album(1, 'Artpop', 2013, 1)
     assert album.id == 1
     assert album.title == 'Artpop'
     assert album.release_year == 2013
-    assert album.genre == 'Pop'
+    assert album.artist_id == 1
+
+"""
+We can format albums to strings nicely
+"""
+def test_albums_format_nicely():
+    album = Album(1, "Test Artist", 2000, 1)
+    assert str(album) == "Album(1, Test Artist, 2000, 1)"
+    # Try commenting out the `__repr__` method in lib/artist.py
+    # And see what happens when you run this test again.
+
+"""
+We can compare two identical albums
+And have them be equal
+"""
+def test_albums_are_equal():
+    album_1 = Album(1, "Test Artist", 2000, 1)
+    album_2 = Album(1, "Test Artist", 2000, 1)
+    assert album_1 == album_2
+    # Try commenting out the `__eq__` method in lib/artist.py
+    # And see what happens when you run this test again.
